d1cce91a6dd0dcf9916715e01acdcd376d128aca
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-05-10 22:16:41 -0300

    Integrate SpeechRecognizer module with Pocketsphinx library
    - Use Pocketsphinx version from commit 05b866c

--- speech_recognizer.h
+++ speech_recognizer.h
@@ -2,13 +2,16 @@
 #define SPEECH_RECOGNIZER_H
 
 #include "scene/main/node.h"
+
 #include "sphinxbase/err.h"
 #include "sphinxbase/ad.h"
+#include "pocketsphinx.h"
 
 class SpeechRecognizer : public Node {
     OBJ_TYPE(SpeechRecognizer, Node);
 
     ad_rec_t *recorder;
+    ps_decoder_t *decoder;
 
 protected:
     static void _bind_methods();
