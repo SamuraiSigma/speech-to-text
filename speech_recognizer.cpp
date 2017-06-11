17186bd01a9a2aaf5b524dbc35e8e64bec8d852d
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-06-11 20:24:15 -0300

    Implement keyword recognition (print keywords on stdout)
    - Use a thread for the speech recognition loop

--- speech_recognizer.cpp
+++ speech_recognizer.cpp
@@ -1,5 +1,8 @@
+#include <cstdio>
 #include "speech_recognizer.h"
-#include "core/os/memory.h"     // memalloc(), memfree()
+
+#include "core/os/memory.h"  // memalloc(), memfree(), memdelete()
+#include "core/os/os.h"      // OS::delay_usec()
 
 /*
  * Adds the -adcdev option as a possible command line argument.
@@ -15,7 +18,8 @@ static arg_t cont_args_def[] = {
     CMDLN_EMPTY_OPTION
 };
 
-void SpeechRecognizer::config(String hmm_dirname, String dict_filename, String kws_filename) {
+void SpeechRecognizer::config(String hmm_dirname, String dict_filename,
+                              String kws_filename) {
     this->hmm_dirname = hmm_dirname;
     this->dict_filename = dict_filename;
     this->kws_filename = kws_filename;
@@ -33,8 +37,10 @@ void SpeechRecognizer::config(String hmm_dirname, String dict_filename, String k
     // Convert each filename: String -> wchar_t * -> char *
     for (int i = 0; i < 3; i++) {
         int len = wcstombs(NULL, names[i].c_str(), 0);
-        if (len == -1)
-            throw "wcstombs(): Can't represent '" + names[i] + "' as a multibyte sequence!";
+        if (len == -1) {
+            throw "wcstombs(): Can't represent '" + names[i] + "' as a "
+                "multibyte sequence!";
+        }
 
         convert[i] = (char *) memalloc((len + 1) * sizeof(char));
         if (convert[i] == NULL)
@@ -45,10 +51,10 @@ void SpeechRecognizer::config(String hmm_dirname, String dict_filename, String k
 
     // Create configuration variable
     conf = cmd_ln_init(NULL, ps_args(), TRUE,
-                       "-hmm", convert[0],
-                       "-dict", convert[1],
-                       "-kws", convert[2],
-                       NULL);
+                        "-hmm", convert[0],
+                        "-dict", convert[1],
+                        "-kws", convert[2],
+                        NULL);
 
     if (conf == NULL)
         throw "Failed to create configuration object!";
@@ -60,7 +66,7 @@ void SpeechRecognizer::config(String hmm_dirname, String dict_filename, String k
 
     // Create recorder variable
     recorder = ad_open_dev(cmd_ln_str_r(conf, "-adcdev"),
-                           (int) cmd_ln_float32_r(conf, "-samprate"));
+                            (int) cmd_ln_float32_r(conf, "-samprate"));
 
     if (recorder == NULL)
         throw "Failed to open audio device!";
@@ -72,15 +78,101 @@ void SpeechRecognizer::config(String hmm_dirname, String dict_filename, String k
         throw "Failed to create decoder!";
 }
 
+void SpeechRecognizer::run() {
+    if (conf == NULL || recorder == NULL || decoder == NULL)
+        throw "Must configure speech recognizer with config() first!";
+
+    is_running = true;
+    recognition = Thread::create(SpeechRecognizer::thread_recognize, this);
+}
+
+void SpeechRecognizer::stop() {
+    if (is_running) {
+        is_running = false;
+        Thread::wait_to_finish(recognition);
+    }
+
+    if (recognition != NULL) {
+        memdelete(recognition);
+        recognition = NULL;
+    }
+}
+
+void SpeechRecognizer::thread_recognize(void *s) {
+    SpeechRecognizer *self = (SpeechRecognizer *) s;
+    self->recognize();
+}
+
+void SpeechRecognizer::recognize() {
+    int16 buffer[BUFFER_SIZE];
+    uint8 utt_started = FALSE;
+    int32 n;
+
+    const char *hyp;
+
+    // Start recording
+    if (ad_start_rec(recorder) < 0) {
+        is_running = false;
+        throw "Failed to start recording";
+    }
+
+    // Start utterance
+    if (ps_start_utt(decoder) < 0) {
+        is_running = false;
+        throw "Failed to start utterance";
+    }
+
+    while (is_running) {
+        // Read data from microphone
+        if ((n = ad_read(recorder, buffer, BUFFER_SIZE)) < 0) {
+            is_running = false;
+            throw "Failed to read audio\n";
+        }
+
+        // Process captured sound
+        ps_process_raw(decoder, buffer, n, FALSE, FALSE);
+
+        // If speech was captured while in silent state, start new utterance
+        if (ps_get_in_speech(decoder) && !utt_started)
+            utt_started = TRUE;
+
+        // Speech stopped while in an utterance
+        if (!ps_get_in_speech(decoder) && utt_started) {
+            // End this utterance phase
+            ps_end_utt(decoder);
+
+            // Show hypothesis (what was said in microphone)
+            hyp = ps_get_hyp(decoder, NULL);
+            if (hyp != NULL)
+                printf(">> %s\n", hyp);
+
+            // Start new utterance
+            if (ps_start_utt(decoder) < 0) {
+                is_running = false;
+                throw "Failed to start utterance!";
+            }
+
+            utt_started = FALSE;
+        }
+
+        // Wait msecs until next microphone input is captured
+        OS::get_singleton()->delay_usec(SLEEP_TIME * 1000);
+    }
+
+    // Stop recording
+    if (ad_stop_rec(recorder) < 0)
+        throw "Failed to stop recording";
+}
+
 void SpeechRecognizer::_bind_methods() {
     ObjectTypeDB::bind_method("config", &SpeechRecognizer::config);
+    ObjectTypeDB::bind_method("run", &SpeechRecognizer::run);
+    ObjectTypeDB::bind_method("stop", &SpeechRecognizer::stop);
 }
 
 SpeechRecognizer::SpeechRecognizer() {
-    #ifndef DEBUG_ENABLED
-        // Disable Pocketphinx log output
-        err_set_logfp(NULL);
-    #endif
+    // Disable Pocketphinx log output
+    err_set_logfp(NULL);
 
     conf = NULL; recorder = NULL; decoder = NULL;
 
@@ -89,10 +181,19 @@ SpeechRecognizer::SpeechRecognizer() {
     kws_filename  = "";
 
     hmm = NULL; dict = NULL; kws = NULL;
+
+    recognition = NULL;
+    is_running = false;
 }
 
 SpeechRecognizer::~SpeechRecognizer() {
-    // Free sphinx variables
+    if (is_running) {
+        is_running = false;
+        Thread::wait_to_finish(recognition);
+    }
+    if (recognition != NULL)
+        memdelete(recognition);
+
     if (recorder != NULL) ad_close(recorder);
     if (decoder  != NULL) ps_free(decoder);
     if (conf     != NULL) cmd_ln_free_r(conf);
--- speech_recognizer.h
+++ speech_recognizer.h
@@ -2,14 +2,22 @@
 #define SPEECH_RECOGNIZER_H
 
 #include "scene/main/node.h"
+#include "core/os/thread.h"
 
 #include "sphinxbase/err.h"
 #include "sphinxbase/ad.h"
 #include "pocketsphinx.h"
 
+// Microphone recorder buffer size
+#define BUFFER_SIZE 2048
+
+// Time, in msecs, to wait before listening to microphone again
+#define SLEEP_TIME 100
+
 class SpeechRecognizer : public Node {
     OBJ_TYPE(SpeechRecognizer, Node);
 
+private:
     cmd_ln_t *conf;         // Configuration type for sphinx variables
     ad_rec_t *recorder;     // Records sound from microphone
     ps_decoder_t *decoder;  // Decodes speech to text
@@ -21,14 +29,46 @@ class SpeechRecognizer : public Node {
     // C strings for the above String names
     char *hmm, *dict, *kws;
 
+    Thread *recognition; // Used to run the speech recognition in parallel
+    bool is_running;     // If true, speech recognition loop is currently on
+
+    /*
+     * Thread wrapper function, calls recognize() method of its SpeechRecognizer
+     * argument.
+     */
+    static void thread_recognize(void *speech_recog);
+
+    /*
+     * Repeatedly listens to keywords from the user's microphone input.
+     */
+    void recognize();
+
 protected:
+    /*
+     * Needed so that GDScript can recognize public methods from this class.
+     */
     static void _bind_methods();
 
 public:
     /*
-     * Creates a configuration object for recognizing speech.
+     * Creates a configuration object for recognizing speech. Receives a directory
+     * containing files for the Hidden Markov Model, a dictionary with words from
+     * the desired language and a keywords file specifying keywords and their
+     * threshold values. These files must follow Pocketsphinx conventions.
      */
     void config(String hmm_dirname, String dict_filename, String kws_filename);
+
+    /*
+     * Creates a thread to repeatedly listen to keywords. Must call config()
+     * first, or it will fail. The thread can be stopped with the stop() method.
+     */
+    void run();
+
+    /*
+     * Stops a created thread that is running the run() method. If run() wasn't
+     * called previously, this function does nothing.
+     */
+    void stop();
 
     /*
      * Initializes speech recognizer variables.
