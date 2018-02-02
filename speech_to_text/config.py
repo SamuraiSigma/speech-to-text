6778a815918644f9ab4ec88cb551c7d9a3577d93
Leonardo Pereira Macedo <lpmacedo95@gmail.com>
2018-02-02 15:12:11 -0200

    Add (empty) xml documentation files for STT classes

--- speech_to_text/config.py
+++ speech_to_text/config.py
@@ -14,3 +14,16 @@ def can_build(platform):
 
 def configure(env):
     pass
+
+
+def get_doc_classes():
+    return [
+        "STTConfig",
+        "STTQueue",
+        "STTRunner",
+        "STTError",
+    ]
+
+
+def get_doc_path():
+    return "doc"
